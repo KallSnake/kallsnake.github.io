3740c0fb8c35fd7d430db5c553712957e811d1d9
Luis Carlos da Silva <34668802+KallSnake@users.noreply.github.com>
2019-02-22 19:46:48 -0300

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -21,7 +21,7 @@ function scrollToPosition(to) {
 
 function scrollToIdOnClick(event) {
 	event.preventDefault();
-	const to = getScrollTopByHref(event.currentTarget) - 30;
+	const to = getScrollTopByHref(event.currentTarget) - 35;
 	scrollToPosition(to);
 }
 
